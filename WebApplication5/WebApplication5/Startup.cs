4c47a6d0913eb80157773a78a2ec2b7aaf80d6de
d4n0n_myself <danon.sibaev@gmail.com>
2019-02-16 20:29:01 +0300

    No more manual, startup finally looking good

--- WebApplication5/WebApplication5/Startup.cs
+++ WebApplication5/WebApplication5/Startup.cs
@@ -1,14 +1,10 @@
-﻿using System.Data;
-using System.IO;
-using System.Linq;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using WebApplication5.Controllers;
+using WebApplication5.Routers;
 
 namespace WebApplication5
 {
@@ -24,7 +20,9 @@ namespace WebApplication5
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddRouting();
+            services.AddMvc(options => options.EnableEndpointRouting = false)
+                    .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,62 +37,24 @@ namespace WebApplication5
                 app.UseHsts();
             }
 
-            #region Mappers
-
-            app.Use((context, next) =>
-            {
-                if (!context.Request.Cookies.ContainsKey("login") &&
-                    !context.Request.Path.StartsWithSegments(new PathString("/Home/Authentificate")))
-                    context.Response.Redirect("/Home/Authentificate");
-                if (context.Request.Cookies.ContainsKey("login") &&
-                    context.Request.Path.StartsWithSegments(new PathString("/Home/Authentificate")))
-                    context.Response.Redirect("/Home");
-                return next.Invoke();
-            });
-
-            app.MapWhen(context => context.Request.Path.StartsWithSegments(new PathString("/texts")),
-                builder => builder.Run(async httpContext =>
-                {
-                    var title = httpContext.Request.Query["name"];
-                    using (var controller = new ValuesController())
-                    {
-                        if (!controller.ContainNote(title))
-                            throw new DataException("No such note");
-
-                        var note = controller.GetNote(title);
-
-                        await httpContext.Response.WriteAsync($@"<a href=""showfile/{note.FileLink}"">File</a>");
-                        await httpContext.Response.WriteAsync("\r\n");
-                        await httpContext.Response.WriteAsync(note.Body);
-                    }
-                }));
-
-            app.MapWhen(context => context.Request.Path.StartsWithSegments(new PathString("/showfile")),
-                builder => builder.Run(async context =>
-                {
-                    var filePath = $"files/{context.Request.Path.ToUriComponent().Split('/').Last()}";
-
-                    if (File.Exists(filePath))
-                        using (var file = File.Open(filePath, FileMode.Open))
-                            file.CopyTo(context.Response.Body);
-                }));
-
-            #endregion
-
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
+            app.UseMiddleware<AuthentificationMiddleware>();
+            
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
                 routes.MapRoute(
-                    "Default", // Route name
-                    "", // URL with parameters
-                    new {controller = "Home", action = "Index"} // Parameter defaults
+                    "EmptyDefault", 
+                    "", 
+                    new {controller = "Home", action = "Index"}
                 );
+                routes.Routes.Add(new FileRouter());
+                routes.Routes.Add(new TextRouter());
             });
         }
     }
